27ba8847132a82c6e43bbc60dccba8cec09c2bac
Andrei <andreistefanariana@gmail.com>
2025-05-16 13:25:16 +0100

    m

--- frontend/src/UserComponents/BotTraining.js
+++ frontend/src/UserComponents/BotTraining.js
@@ -83,7 +83,8 @@ const SupportBotCustomization = () => {
           { username: user.username },
           { withCredentials: true }
         );
-  
+        console.log("API response config:", response.data.config);
+
         const data = response.data.config || {};
   
         const mappedConfig = {
