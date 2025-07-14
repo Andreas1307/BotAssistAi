0be1db057526195a63f4d7547e3350b535b4a302
Andrei <andreistefanariana@gmail.com>
2025-07-14 22:20:15 +0100

    nuhibkjnkjnjk

--- frontend/src/utils/AppBridgeProvider.jsx
+++ frontend/src/utils/AppBridgeProvider.jsx
@@ -1,14 +1,14 @@
 // components/AppBridgeProvider.jsx
 import React from "react";
-import { AppBridgeProvider as ShopifyAppBridgeProvider } from "@shopify/app-bridge-react";
+import AppBridgeReactProvider from "@shopify/app-bridge-react";
 import { AppProvider as PolarisProvider } from "@shopify/polaris";
 
 export const AppBridgeProvider = ({ config, children }) => {
   return (
-    <ShopifyAppBridgeProvider config={config}>
+    <AppBridgeReactProvider config={config}>
       <PolarisProvider>
         {children}
       </PolarisProvider>
-    </ShopifyAppBridgeProvider>
+    </AppBridgeReactProvider>
   );
 };
