5a4e6f7f7439c4f867f896f0378cc14b7f99ba99
Andrei <andreistefanariana@gmail.com>
2025-07-14 22:57:36 +0100

    adsajdnasjdsa

--- frontend/src/utils/AppBridgeProvider.jsx
+++ frontend/src/utils/AppBridgeProvider.jsx
@@ -1,17 +1,14 @@
 // components/AppBridgeProvider.jsx
 import React, { useMemo } from "react";
-import { Provider as AppBridgeReactProvider } from "@shopify/app-bridge-react";
 import { AppProvider as PolarisProvider } from "@shopify/polaris";
+import createApp from "@shopify/app-bridge";
 
 export const AppBridgeProvider = ({ config, children }) => {
-  // useMemo ensures the appBridge config doesn't get recreated on every render
-  const appBridgeConfig = useMemo(() => config, [config]);
+  const app = useMemo(() => createApp(config), [config]);
 
   return (
-    <AppBridgeReactProvider config={appBridgeConfig}>
-      <PolarisProvider>
-        {children}
-      </PolarisProvider>
-    </AppBridgeReactProvider>
+    <PolarisProvider>
+      {children}
+    </PolarisProvider>
   );
 };
