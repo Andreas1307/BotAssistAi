a3e9ab3db145895a36e5ec5baf964fa07348ecee
Andrei <andreistefanariana@gmail.com>
2025-07-24 17:23:38 +0100

    dfFSDFds

--- backend/sessionStorage.js
+++ backend/sessionStorage.js
@@ -18,7 +18,7 @@ function saveSessions(sessions) {
 module.exports = {
   storeSession: async (session) => {
     const sessions = loadSessions();
-    const normalizedShop = session.shop.toLowerCase().replace(/^https:\/\//, '');
+    const normalizedShop = session.shop.toLowerCase().replace(/^https:\/\//, '').replace(/\/$/, '');
     const sessionId = session.id;
     sessions[sessionId] = session;
     saveSessions(sessions);
@@ -27,7 +27,7 @@ module.exports = {
 
   findSessionsByShop: async (shop) => {
     const sessions = loadSessions();
-    const normalized = shop.toLowerCase().replace(/^https:\/\//, '');
+    const normalized = shop.toLowerCase().replace(/^https:\/\//, '').replace(/\/$/, '');
     return Object.values(sessions).filter(s => {
       const storedShop = s.shop?.toLowerCase().replace(/^https:\/\//, '');
       return storedShop === normalized;
