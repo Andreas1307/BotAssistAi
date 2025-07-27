6e3a762dec1653ee670ff27aaa04335d13cbb200
Andrei <andreistefanariana@gmail.com>
2025-07-27 08:03:05 +0100

    Fdsffdsfssdfs

--- backend/shopify.js
+++ backend/shopify.js
@@ -1,14 +1,14 @@
 const { shopifyApi, LATEST_API_VERSION, LogSeverity } = require("@shopify/shopify-api");
 const customSessionStorage = require("./sessionStorage");
 
-if (!process.env.SCOPES) {
+if (!process.env.SHOPIFY_SCOPES) {
   throw new Error("❌ Missing SCOPES in environment variables.");
 }
 
 const shopify = shopifyApi({
   apiKey: process.env.SHOPIFY_API_KEY,
   apiSecretKey: process.env.SHOPIFY_API_SECRET,
-  scopes: process.env.SCOPES.split(","),
+  scopes: process.env.SHOPIFY_SCOPES.split(","),
   hostName: process.env.HOST.replace(/https?:\/\//, ""),
   apiVersion: LATEST_API_VERSION,
   isEmbeddedApp: true,
