1a31be4bf12f7235b150d674528af97f8c90ccfb
Andrei <andreistefanariana@gmail.com>
2025-07-29 10:00:17 +0100

    fddsffsd

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import { initShopifyAppBridge } from './initShopifyAppBridge';
+import { initShopifyAppBridge } from './utils/initShopifyAppBridge';
 
 import Homepage from './pages/homepage';
 import Error from './pages/errorPage';
