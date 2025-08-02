6cbc35ecdb4ec1c62474c00329d58c2cd6932355
Andrei <andreistefanariana@gmail.com>
2025-08-02 13:25:13 +0100

    fdfdsfsd

--- backend/server.js
+++ backend/server.js
@@ -889,25 +889,8 @@ app.get("/auth/callback", async (req, res) => {
         console.log('✅ Password email sent:', info.response);
       });
 
-const responseUninstall = await client.post({
-    path: "webhooks",
-    data: {
-      webhook: {
-        topic: "APP_UNINSTALLED",
-        address: `${process.env.HOST}/shopify/uninstall`,
-        format: "json",
-      },
-    },
-    type: "json",
-  });
-
-  console.log("✅ Uninstall webhook registered:", responseUninstall?.body);
-
     }
-   
-
-
-     
+  
 
 
 
