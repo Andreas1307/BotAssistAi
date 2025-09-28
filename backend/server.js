83ca4244fdb2cb1a74bc6779d97693f7e2c3216e
Andrei <andreistefanariana@gmail.com>
2025-09-28 09:15:39 +0100

    cxvdsrf3r443

--- backend/server.js
+++ backend/server.js
@@ -1123,9 +1123,10 @@ app.get("/shopify/callback", async (req, res) => {
             });
             const redirect = actions.Redirect.create(app);
             redirect.dispatch(
-              actions.Redirect.Action.APP,
-              '/dashboard?shop=${encodeURIComponent(shop)}&host=${encodeURIComponent(host)}'
-            );
+  actions.Redirect.Action.APP,
+  '/${user.username}/dashboard?shop=${encodeURIComponent(shop)}&host=${encodeURIComponent(host)}'
+);
+
           </script>
         </body>
       </html>
