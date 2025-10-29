485593b6f3fd5a6f2b1ff2add6a1c0dd47590e57
Andrei <andreistefanariana@gmail.com>
2025-10-29 12:13:26 +0000

    vcxvxcfwerTFWE

--- backend/shopify.js
+++ backend/shopify.js
@@ -1,17 +1,14 @@
-// --- Ensure NODE_ENV/ENV set:
-// HOST = "https://api.botassistai.com"
-// SHOPIFY_API_KEY, SHOPIFY_API_SECRET set
 const { shopifyApi, LATEST_API_VERSION } = require('@shopify/shopify-api');
 require('@shopify/shopify-api/adapters/node');
-const { storeCallback, loadCallback, deleteCallback } = require('./sessionStorage');
 
 const shopify = shopifyApi({
   apiKey: process.env.SHOPIFY_API_KEY,
   apiSecretKey: process.env.SHOPIFY_API_SECRET,
   scopes: process.env.SHOPIFY_SCOPES.split(','),
-  hostName: "api.botassistai.com", // ✅ NO protocol, must match your API host exactly
+  hostName: "api.botassistai.com",
   apiVersion: LATEST_API_VERSION,
   isEmbeddedApp: true,
   sessionStorage: { storeCallback, loadCallback, deleteCallback },
 });
+
 module.exports = { shopify };
