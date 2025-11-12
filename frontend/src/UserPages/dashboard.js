7140e92eac2eeefb873e5ac64620b404674f21c8
Andrei <andreistefanariana@gmail.com>
2025-11-12 23:59:50 +0000

    DfsdfsCVX

--- frontend/src/UserPages/dashboard.js
+++ frontend/src/UserPages/dashboard.js
@@ -270,6 +270,7 @@ const Dashboard = () => {
       try {
         const res = await fetchWithAuth("/auth-check"); 
         setUser(res.user);
+        console.log("YOOOOO", res.user)
         if(res.user.shopify_access_token) {
           setShopifyUser(true)
         } else {
