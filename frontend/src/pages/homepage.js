d1feb0d50bf9e2ea541092f766e5630746f3da18
Andrei <andreistefanariana@gmail.com>
2025-11-12 23:29:32 +0000

    FDSfdscvx

--- frontend/src/pages/homepage.js
+++ frontend/src/pages/homepage.js
@@ -19,6 +19,7 @@ import axios from "../utils/axiosShopify";
 import { safeRedirect, initShopifyAppBridge, fetchWithAuth } from "../utils/initShopifyAppBridge";
 import { Helmet } from "react-helmet";
 import { getAppBridgeInstance } from "../utils/initShopifyAppBridge";
+import { Redirect } from "@shopify/app-bridge/actions";
 
 const Homepage = () => {
   const [stars, setStars] = useState([]);
