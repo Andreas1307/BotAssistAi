639b286d93dac0f8b652e364bfcacde58cb2c704
Andrei <andreistefanariana@gmail.com>
2025-11-16 23:47:07 +0000

    dfsadfcV

--- frontend/src/utils/loadAppBridge.js
+++ frontend/src/utils/loadAppBridge.js
@@ -1,26 +1,8 @@
-//inside loadAppBridge.js
-import React from "react";
-
 export function loadAppBridge(apiKey, callback) {
-    if (window.ShopifyAppBridge && !window.appBridgeInitialized) {
-      window.appBridgeInitialized = true;
+    if (window.ShopifyAppBridge) {
       callback(window.ShopifyAppBridge);
-      return;
+    } else {
+      console.error("App Bridge not found on window.");
     }
-  
-    const script = document.createElement("script");
-    script.src = "https://cdn.shopify.com/shopifycloud/app-bridge.js";
-    script.async = true;
-  
-    script.onload = () => {
-      window.appBridgeInitialized = true;
-      callback(window.ShopifyAppBridge);
-    };
-  
-    script.onerror = (err) => {
-      console.error("Failed to load Shopify App Bridge script:", err);
-    };
-  
-    document.head.appendChild(script);
   }
   
