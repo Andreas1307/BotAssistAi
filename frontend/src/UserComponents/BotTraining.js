64184c053b65c2f08d56d014f5009f668dfbc22a
Andrei <andreistefanariana@gmail.com>
2025-12-07 15:46:37 +0000

    FDSdsffdsVCX

--- frontend/src/UserComponents/BotTraining.js
+++ frontend/src/UserComponents/BotTraining.js
@@ -172,6 +172,7 @@ const setFieldValue = (field, value) => {
     formData.append("webUrl", userData.web_url ?? "");
     formData.append("phoneNum", userData.phoneNum ?? "");
   
+    console.log("FORM DATA", formData)
     try {
       const response = await fetchWithAuth(`/update-config`, {
         method: "POST",
