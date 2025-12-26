84d3121d18356d62dfd81aef04f377b9e8c1de53
Andrei <andreistefanariana@gmail.com>
2025-12-26 19:45:24 +0000

    fdsfsdREWRWE

--- backend/server.js
+++ backend/server.js
@@ -1409,6 +1409,7 @@ return res.status(200).send(`
 <html>
   <head>
     <meta charset="utf-8"/>
+     <meta name="shopify-api-key" content="${SHOPIFY_API_KEY}" />
    <script src="https://cdn.shopify.com/shopifycloud/app-bridge.js"></script>
   </head>
   <body>
