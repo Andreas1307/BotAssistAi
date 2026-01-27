b3cba0efc150ef80f0360ccd1b888d8e16ef51fd
Andrei <andreistefanariana@gmail.com>
2026-01-27 13:08:11 +0000

    FDSDSF

--- backend/public/client-chatbot.js
+++ backend/public/client-chatbot.js
@@ -260,7 +260,7 @@ clear: both;
   const chatBotHeader = document.createElement("div")
   chatBotHeader.style.cssText = `
     width: 102%;
-    margin-left: -3px;
+    margin-left: -3.5px;
   display: flex;
   align-items: center;
   justify-content: space-between;
